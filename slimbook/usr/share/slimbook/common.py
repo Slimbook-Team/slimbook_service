e22f77d963370c025418dc3c862094547a1b6f2b
samantaslimbook <samantasanchez@slimbook.com>
2025-03-17 10:18:01 +0100

    remove useless import @ common

--- slimbook/usr/share/slimbook/common.py
+++ slimbook/usr/share/slimbook/common.py
@@ -18,7 +18,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import time
 import slimbook.info
 import slimbook.smbios
 
@@ -28,7 +27,6 @@ import locale
 import gettext
 import requests
 import re
-
 import signal
 
 try:
